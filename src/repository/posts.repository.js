2c789840a6566046ec202eeb39a6134cedc80ff8
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-28 20:55:25 -0300

    fix: complementing get posts response

--- src/repository/posts.repository.js
+++ src/repository/posts.repository.js
@@ -18,9 +18,11 @@ export function getPostsByUserId(id,offset,userId){
 }
 
 export function getPostByIdRepository(id,userId){
-    return db.query(`SELECT posts.*,
+    return db.query(`SELECT posts.*,users.username AS username, users.photo AS userphoto,
     (SELECT COUNT(likes.id) AS like FROM likes WHERE likes.postid = posts.id ),
     (SELECT COUNT(comments.id) AS comment FROM comments WHERE comments.postid = posts.id ),
     (SELECT COUNT(likes.id) AS isliked FROM likes WHERE likes.userid = $2 AND likes.postid = posts.id ) 
-    FROM posts WHERE posts.id = $1`,[id,userId])
+    FROM posts 
+    JOIN users ON posts.userid = users.id
+    WHERE posts.id = $1`,[id,userId])
 }
